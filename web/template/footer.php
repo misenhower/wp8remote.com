e25ba51ee78ef3f4b087ce093085ed2a96a055d3
Matt Isenhower <matt@isenhower.com>
2011-01-03 15:04:34 -0700

    Adding Twitter link

--- web/template/footer.php
+++ web/template/footer.php
@@ -3,9 +3,15 @@
 		<a href="http://komodex.com/" class="komolink"><img src="/images/komodex-38.png" /></a>
 		<div>
 			<a href="/">Home</a> -
-			<a href="/utility">Download Pairing Utility</a> -
+			<a href="/utility">Pairing Utility</a> -
 			<a href="/troubleshooting">Troubleshooting</a> -
-			<a href="<?= KOMO_CONTACT_LINK ?>">Contact Us</a>
+			<a href="<?= KOMO_CONTACT_LINK ?>">Contact Us</a> -
+			<span class="twitterlink">
+				<a href="http://twitter.com/WP7Remote">
+					<span class="i unitPng">&nbsp;</span>
+					<span class="t">Twitter</span>
+				</a>
+			</span>
 		</div>
 <? if (!defined('KOMO_HIDE_BOTTOM_MARKET_LINK')): ?>
 		<a href="<?= KOMO_MARKETPLACE_LINK ?>" target="_blank" style="float:right"><img src="/images/wp7-38.png" /></a>
