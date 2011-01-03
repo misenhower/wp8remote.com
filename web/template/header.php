2288944359792ec05907dc1e0d6879c9b52ded4a
Matt Isenhower <matt@isenhower.com>
2011-01-03 14:15:22 -0700

    Adding RemoteIcon for page thumbnail

--- web/template/header.php
+++ web/template/header.php
@@ -15,6 +15,7 @@ var clear="http://komodex.com/wordpress/wp-content/themes/komodex/images/clear.g
 </script>
 <script type="text/javascript" src="http://komodex.com/wordpress/wp-content/themes/komodex/js/unitpngfix.js"></script>
 <![endif]-->
+<link rel="image_src" href="http://wp7remoteapp.com/images/RemoteIcon.png" />
 <meta name="description" content="With Remote, you can control iTunes from your Windows Phone 7 device.">
 <meta name="keywords" content="iTunes, remote, windows phone 7, iTunes remote, windows phone 7 app">
 </head>
