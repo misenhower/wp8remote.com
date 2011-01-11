40d4e80704d4b7c5374738e63b0bd258e5bf4697
Matt Isenhower <matt@isenhower.com>
2011-01-10 21:16:10 -0700

    Added copyright line

--- web/template/footer.php
+++ web/template/footer.php
@@ -16,6 +16,7 @@
 <? if (!defined('KOMO_HIDE_BOTTOM_MARKET_LINK')): ?>
 		<a href="<?= KOMO_MARKETPLACE_LINK ?>" target="_blank" style="float:right"><img src="/images/wp7-38.png" /></a>
 <? endif; ?>
+	<div class="copy"><em>Copyright <?= date('Y'); ?> Komodex Systems LLC. All rights reserved. All trademarks are the property of their respective owners.</em></div>
 	</div>
 </div>
 </body>
