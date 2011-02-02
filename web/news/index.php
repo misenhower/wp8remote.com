cd31f96e2210495b99f46b1ff8557e65f6ca2387
Matt Isenhower <matt@isenhower.com>
2011-02-02 15:16:07 -0700

    Updating News page for 1.2 release

--- web/news/index.php
+++ web/news/index.php
@@ -1,13 +1,21 @@
 <? set_include_path(get_include_path() . PATH_SEPARATOR . '../template'); ?>
-<? /*define('KOMO_LONGPAGE', true);*/ ?>
+<? define('KOMO_LONGPAGE', true); ?>
 <? define('KOMO_TITLE', 'News'); ?>
 <? require('header.php'); ?>
 <h2>Product News &amp; Updates</h2>
+<h3>Version 1.2</h3>
+<ul>
+	<li><strong>Added option to stay connected to iTunes while the phone is locked.</strong></li>
+	<li>Added Settings page.</li>
+	<li>Minor UI improvements.</li>
+</ul>
+<hr />
 <h3>Version 1.1</h3>
 <ul>
 	<li><strong>Added AirPlay control support.</strong> Tap the icon next to the volume bar on the Now Playing page to control which speakers iTunes sends music to.</li>
 	<li><strong>Added movies, TV shows, and podcasts.</strong></li>
 	<li>Fixed issue where the reconnecting screen would appear approx. every 60 seconds.</li>
+	<li>Fixed issue where the Now Playing page wouldn't be displayed after picking a song on slower networks.</li>
 	<li>Fixed issues with changing the volume and play position on slower networks.</li>
 	<li>The Choose Library page now displays the host and PIN for each library for easier identification.</li>
 	<li>List and other miscellaneous performance improvements.</li>
@@ -19,6 +27,6 @@
 <hr />
 <h3>Version 1.0</h3>
 <ul>
-	<li>First release.</li>
+	<li>Initial release.</li>
 </ul>
 <? require('footer.php'); ?>
