3e5a23af16d77f9bca732037679b1b9a2bbf83d6
Matt Isenhower <matt@isenhower.com>
2011-02-17 09:04:06 -0700

    Cache blocking CSS

--- web/template/header.php
+++ web/template/header.php
@@ -13,7 +13,7 @@ define('KOMO_PAIRINGUTILITY_MAC_SIZE', '332kb');
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title>Remote for Windows Phone 7<? if (defined('KOMO_TITLE')) print " &mdash; " . KOMO_TITLE; ?></title>
-<link rel="stylesheet" href="/css/style.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="/css/style.css?1" type="text/css" media="screen" />
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.0/jquery.min.js"></script>
 <script type="text/javascript" src="/js/jquery.cycle.all.min.js"></script>
 <script type="text/javascript" src="/js/jquery.easing.1.3.js"></script>
