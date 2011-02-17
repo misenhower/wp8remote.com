cf56eadd8be63984b46c5bc2f5db750c8efadab3
Matt Isenhower <matt@isenhower.com>
2011-02-16 18:37:54 -0700

    Added Foobar page

--- web/help/foobar/index.php
+++ web/help/foobar/index.php
@@ -1,66 +1,22 @@
 <? set_include_path(get_include_path() . PATH_SEPARATOR . '../../template'); ?>
 <? define('KOMO_LONGPAGE', true); ?>
-<? define('KOMO_TITLE', 'iTunes Setup Guide'); ?>
+<? define('KOMO_TITLE', 'Foobar2000 Setup Guide'); ?>
 <? define('KOMO_PAGETITLE', 'Setup Guide'); ?>
 <? require('header.php'); ?>
-<?
-$is_mac = !(strpos($_SERVER['HTTP_USER_AGENT'], 'Mac OS X') === false);
-
-if (!$is_mac)
-	$mac = 'style="display:none;"';
-else
-	$mac = '';
-
-?>
 <script type="text/javascript">
 var showIPaddresses = false;
-
-$(document).ready(function() {
-	$('.helplinks a.winselector, .helplinks a.macselector').hover(
-		function() {
-			$('.helplinks .selected').addClass('fakehover');
-		},
-		function () {
-			$('.helplinks .selected').removeClass('fakehover');
-		}
-	);
-	
-	$('.helplinks a.winselector').click(function() {
-		$('.mac').fadeOut();
-		if (showIPaddresses)
-			$('.ip').show();
-		else
-			$('.ip').hide();
-		$('.helplinks a.winselector').removeClass('unsel');
-		$('.helplinks a.macselector').addClass('unsel');
-		return false;
-	});
-	
-	$('.helplinks a.macselector').click(function() {
-		$('.mac').fadeIn();
-		if (showIPaddresses)
-			$('.ip').show();
-		else
-			$('.ip').hide();
-		$('.helplinks a.winselector').addClass('unsel');
-		$('.helplinks a.macselector').removeClass('unsel');
-		return false;
-	});
-});
 </script>
 <div style="overflow:hidden; margin-bottom:15px">
 <div class="helplinks hl2">
 	<span class="inner">
 		<div style="float:right;">
-			<span class="app selected">
-				<a href="#" class="right winselector<? if ($is_mac) print(' unsel'); ?>"><img src="/images/app-itunes-win.png" class="unitPng" /></a>
-				<a href="#" class="right macselector<? if (!$is_mac) print(' unsel'); ?>"><img src="/images/app-itunes-mac.png" class="unitPng" /></a>
+			<span class="app">
 				<a href="/help/itunes/" class="main">
 					<img src="/images/app-itunes.png" class="unitPng" />
 					<span>iTunes</span><!--
 			 --></a>
 			</span><!--
-		 --><span class="app">
+		 --><span class="app selected">
 				<a href="/help/foobar/" class="main">
 					<img src="/images/app-foobar.png" class="unitPng" />
 					<span>Foobar2000</span>
@@ -74,7 +30,7 @@ $(document).ready(function() {
 		</span>
 		</div>
 		<span class="inst">
-			<h2>iTunes Setup Guide</h2>
+			<h2>Foobar2000 Setup Guide</h2>
 		</span>
 	</span>
 </div>
@@ -83,7 +39,6 @@ $(document).ready(function() {
 <div class="step">
 <div class="img">
 	<img src="/images/instructions/itunes-win-pairingutil1.png" class="abs win" />
-	<img src="/images/instructions/itunes-mac-pairingutil1.png" class="abs mac" <?= $mac ?> />
 </div>
 <h3>1. Download and run the Pairing Utility</h3>
 <div class="details"><a href="#">Click here</a> to download the Pairing Utility.  After downloading, open or run the utility.</div>
@@ -92,30 +47,14 @@ $(document).ready(function() {
 <div class="step">
 <div class="img">
 	<img src="/images/instructions/itunes-win-wp7d1.png" class="abs win" />
-	<img src="/images/instructions/itunes-mac-wp7d1.png" class="abs mac" <?= $mac ?> />
 </div>
 <h3>2. Click "Windows Phone 7 Device" in iTunes</h3>
 <div class="details">This will appear under the "Devices" header on the left side of the window.</div>
-<div class="details">
-	<h4><a href="#" id="q1a">I don't see "Windows Phone 7 Device" in the sidebar</a></h4>
-	<div class="issue" id="q1b" style="position:relative;">
-		<div class="mac" style="position:absolute; background-color:#131417;">Open the iTunes Preferences window (<strong>iTunes</strong> &rarr; <strong>Preferences</strong> from the menu bar),  go to the <strong>Devices</strong> tab, and make sure the option that says <strong>Look for iPod touch, iPhone and iPad Remotes</strong> is checked.</div>
-		<div>Open the iTunes Preferences window (<strong>Edit</strong> &rarr; <strong>Preferences</strong> at the top of the iTunes window),  go to the <strong>Devices</strong> tab, and make sure the option that says <strong>Look for iPod touch, iPhone and iPad Remotes</strong> is checked.</div>
-	</div>
-	<script type="text/javascript">
-		$('#q1b').hide();
-		$('#q1a').click(function () {
-			$('#q1b').slideToggle();
-			return false;
-		});
-	</script>
-</div>
 </div>
 
 <div class="step">
 <div class="img">
 	<img src="/images/instructions/itunes-win-wp7d2.png" class="abs win" />
-	<img src="/images/instructions/itunes-mac-wp7d2.png" class="abs mac" <?= $mac ?> />
 </div>
 <h3>3. Type the PIN from the Pairing Utility into iTunes</h3>
 </div>
@@ -124,10 +63,6 @@ $(document).ready(function() {
 <div class="img">
 	<img src="/images/instructions/itunes-win-pairingutil2.png" class="abs win" />
 	<img src="/images/instructions/itunes-win-pairingutil3.png" class="abs win ip" style="display:none;" />
-	<div class="abs mac" <?= $mac ?>>
-		<img src="/images/instructions/itunes-mac-pairingutil2.png" class="abs" />
-		<img src="/images/instructions/itunes-mac-pairingutil3.png" class="abs ip" style="display:none;" />
-	</div>
 </div>
 <h3>4. Confirm that pairing completed successfully</h3>
 </div>
@@ -142,10 +77,6 @@ $(document).ready(function() {
 <div class="img">
 	<img src="/images/instructions/itunes-win-phonehost.png" class="abs win" />
 	<img src="/images/instructions/itunes-win-phoneip.png" class="abs win ip" style="display:none;" />
-	<div class="abs mac" <?= $mac ?>>
-		<img src="/images/instructions/itunes-mac-phonehost.png" class="abs" />
-		<img src="/images/instructions/itunes-mac-phoneip.png" class="abs ip" style="display:none;" />
-	</div>
 </div>
 <h3>6. Enter the host and PIN and tap "Save"</h3>
 <div class="details">The phone will save the library details and connect to iTunes.</div>
